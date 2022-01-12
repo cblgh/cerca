4951050e6e03e7e974039c661376cb05fde84ba2
Nat Welch <nat@natwelch.com>
2022-01-12 01:59:15 +0000

    Add time html element

--- server/server.go
+++ server/server.go
@@ -107,7 +107,10 @@ var (
 	templates     = template.Must(template.ParseFiles(wrapViews()...))
 	templateFuncs = template.FuncMap{
 		"formatDateTime": func(dt time.Time) string {
-			return t.Format(time.RFC3339)
+			return t.Format("2006-01-02 15:04:05")
+		},
+		"formatDateTimeRFC3339": func(dt time.Time) string {
+			return t.Format(time.RFC3339Nano)
 		},
 		"formatDate": func(dt time.Time) string {
 			return t.Format("2006-01-02")
