1f23204a2dc5fe3765d265598940666fe2e42d38
cblgh <cblgh@cblgh.org>
2022-02-09 21:12:32 +0100

    add import subtool

---
+++ cmd/css-compiler/import.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "strings"
+  "regexp"
+  "cerca/util"
+)
+
+func main () {
+  b, err := os.ReadFile("./import.css")
+  util.Check(err, "read import.css")
+
+  lines := string(b)
+  imports := collectImports(lines)
+  for _, filepath := range imports {
+    b, err := os.ReadFile(filepath)
+    util.Check(err, "read @imported file")
+    fmt.Println(string(b))
+  }
+}
+
+var importPattern = regexp.MustCompile(`@import\s*"(\S+)";`)
+var altImportPattern = regexp.MustCompile(`@import\s*url\("(\S+)"\);`)
+func collectImports(input string) []string {
+  paths := make([]string, 0)
+  for _, line := range strings.Split(input, "\n") {
+    if strings.HasPrefix(line, "@import") {
+      matches := importPattern.FindStringSubmatch(line)
+      if len(matches) == 0 {
+        matches = altImportPattern.FindStringSubmatch(line)
+      }
+      if len(matches) > 1 {
+        paths = append(paths, matches[1])
+      }
+    }
+  }
+  return paths
+}
