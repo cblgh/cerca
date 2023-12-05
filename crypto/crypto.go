67bcd279d742e706e653a7784f8b952b6661b382
cblgh <cblgh@cblgh.org>
2023-12-05 21:55:46 +0100

    switch argon2 library
    reason / hope is to get away from a mistake in the previous library,
    rendering cerca inoperable on 32 bit architectures like older raspberry
    pis
    
    write down password hash differences between old and new argon dep

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -4,7 +4,8 @@ import (
 	"cerca/util"
 	crand "crypto/rand"
 	"encoding/binary"
-	"github.com/synacor/argon2id"
+	// "github.com/synacor/argon2id"
+	"github.com/matthewhartstonge/argon2"
 	"math/big"
 	rand "math/rand"
 	"strings"
@@ -12,26 +13,58 @@ import (
 
 func HashPassword(s string) (string, error) {
 	ed := util.Describe("hash password")
-	// TODO (2023-12-05): see about argon2id replacement (or getting 32 bit bug fixed)
-	hash, err := argon2id.DefaultHashPassword(s)
+	config := argon2.MemoryConstrainedDefaults()
+	hash, err := config.HashEncoded([]byte(s))
 	if err != nil {
 		return "", ed.Eout(err, "hashing with argon2id")
 	}
-	return hash, nil
+	return string(hash), nil
 }
 
+// TODO (2023-12-05): figure out migration of the password hashes from synacor's embedded salt format to
+// matthewartstonge's key-val embedded format
+
+// migration details: 
+//
+// the old format had the following default parameters:
+// * time = 1
+// * memory = 64MiB
+// * threads = 4
+// * keyLen = 32
+// * saltLen = 16 bytes
+// * hashLen = 32 bytes?
+// * argonVersion = 13?
+// 
+//
+// the new format uses the following parameters:
+// *	HashLength:  32, // 32 * 8 = 256-bits
+// *	SaltLength:  16, // 16 * 8 = 128-bits
+// *	TimeCost:    3,
+// *	MemoryCost:  64 * 1024, // 2^(16) (64MiB of RAM)
+// *	Parallelism: 4,
+// *	Mode:        ModeArgon2id,
+// *	Version:     Version13,
+//
+// the diff:
+// * time was changed to 3 from 1
+// * the version may or may not be the same (0x13)
+//
+// a regex for changing the values would be the following
+// old format example value:
+// $argon2id19$1,65536,4$111111111111111111111111111111111111111111111111111111111111111111
+// diff regex from old to new
+// $argon2id$v=19$m=65536,t=${1},p=4${passwordhash}
+// new format example value: 
+// $argon2id$v=19$m=65536,t=3,p=4$222222222222222222222222222222222222222222222222222222222222222222
 func ValidatePasswordHash(password, passwordHash string) bool {
-	err := argon2id.Compare(passwordHash, password)
+	ed := util.Describe("validate password hash")
+	hashStruct, err := argon2.Decode([]byte(passwordHash))
+	ed.Check(err, "argon2.decode")
+	correct, err := hashStruct.Verify([]byte(password))
 	if err != nil {
 		return false
 	}
-	return true
-}
-
-func GenerateVerificationCode() int {
-	var src cryptoSource
-	rnd := rand.New(src)
-	return rnd.Intn(999999)
+	return correct
 }
 
 // used for generating a random reset password
@@ -50,6 +83,12 @@ func GeneratePassword() string {
 		password.WriteString(string(characterSet[n]))
 	}
 	return password.String()
+}
+
+func GenerateVerificationCode() int {
+	var src cryptoSource
+	rnd := rand.New(src)
+	return rnd.Intn(999999)
 }
 
 type cryptoSource struct{}
