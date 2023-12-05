a92f1616f7c8f6bdc60a4b5e176b57a289f71a33
cblgh <cblgh@cblgh.org>
2023-12-05 20:47:08 +0100

    remove public key concept and password tool
    cleaning up ResetPasswordRoute, at least for the interim

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -34,8 +34,7 @@ var English = map[string]string{
 	"Username":                  "username",
 	"Current":                   "current",
 	"New":                       "new",
-	"GenerateNewKeypair":        "I also want to generate a new keypair",
-	"ChangePasswordDescription": "Use this page to change your password. If needed, you can also regenerate your password reset keypair—used to reset a forgotten password without admin help.",
+	"ChangePasswordDescription": "Use this page to change your password.",
 	"Password":                  "password",
 	"PasswordMin":               "Must be at least 9 characters long",
 	"PasswordForgot":            "Forgot your password?",
@@ -52,7 +51,6 @@ var English = map[string]string{
 	"TextareaPlaceholder": "Tabula rasa",
 
 	"PasswordReset":                   "reset password",
-	"PasswordResetMessage":            "You are logged in, log out to reset password using proof",
 	"PasswordResetSuccess":            "Reset password—success!",
 	"PasswordResetSuccessMessage":     "You reset your password!",
 	"PasswordResetSuccessLinkMessage": "Give it a try and",
@@ -86,9 +84,6 @@ var English = map[string]string{
 	"ThreadStartNew": "Start a new thread",
 
 	"RegisterHTMLMessage":               `You now have an account! Welcome. Visit the <a href="/">index</a> to read and reply to threads, or start a new one.`,
-	"RegisterKeypairExplanationStart":   `There's just one more thing: <b>save the key displayed below</b>. It is a <a href="https://en.wikipedia.org/wiki/Public-key_cryptography">keypair</a> describing your forum identity, with a private part that only you know; the forum only stores the public portion.`,
-	"RegisterViewKeypairExplanationEnd": `With this keypair you will be able to reset your account if you ever lose your password—and without having to share your email (or require email infrastructure on the forum's part).`,
-	"RegisterKeypairWarning":            "This keypair will only be displayed once",
 
 	"RegisterVerificationCode":              "Your verification code is",
 	"RegisterVerificationInstructionsTitle": "Verification instructions",
@@ -96,22 +91,6 @@ var English = map[string]string{
 	"RegisterConductCodeBoxOne":             `I have refreshed my memory of the <a target="_blank" href="{{ .Data.Link }}">{{ .Data.Name }} Code of Conduct</a>`,
 	"RegisterConductCodeBoxTwo":             `Yes, I have actually <a target="_blank" href="{{ .Data.Link }}">read it</a>`,
 
-	"PasswordResetDescription":            "On this page we'll go through a few steps to securely reset your password—without resorting to any emails!",
-	"PasswordResetUsernameQuestion":       "First up: what was your username?",
-	"PasswordResetCopyPayload":            `Now, first copy the snippet (aka <i>proof payload</i>) below`,
-	"PasswordResetFollowToolInstructions": `Follow the <b>tool instructions</b> to finalize the password reset.`,
-	"ToolInstructions":                    `tool instructions`,
-	"PasswordResetToolInstructions": fmt.Sprintf(`
-    <ul>
-        <li><a href="%s">Download the tool</a></li>
-        <li>Run as:<br><code>pwtool --payload &lt;proof payload from above&gt; --keypair &lt;path to file with yr keypair from registration&gt;</code>
-        </li>
-        <li>Copy the generated proof and paste below</li>
-        <li>(Remember to save your password :)</li>
-    </ul>
-    `, toolURL),
-	"GeneratePayload": "generate payload",
-	"Proof":           "proof",
 	"NewPassword":     "new password",
 	"ChangePassword":  "change password",
 }
@@ -140,8 +119,7 @@ var Swedish = map[string]string{
 	"Username":                  "användarnamn",
 	"Current":                   "nuvarande",
 	"New":                       "nytt",
-	"GenerateNewKeypair":        "Jag vill också generera ett nytt nyckelpar",
-	"ChangePasswordDescription": "På den här sidan kan du ändra ditt lösenord. Vid behov kan du också regenerera ditt nyckelpar—används för att nollställa ditt lösenord utan att be admin om hjälp.",
+	"ChangePasswordDescription": "På den här sidan kan du ändra ditt lösenord.",
 	"Password":                  "lösenord",
 	"PasswordMin":               "Måste vara minst 9 karaktärer långt",
 	"PasswordForgot":            "Glömt lösenordet?",
@@ -158,7 +136,6 @@ var Swedish = map[string]string{
 	"TextareaPlaceholder": "Tabula rasa",
 
 	"PasswordReset":                   "nollställ lösenord",
-	"PasswordResetMessage":            "Du är inloggad, logga ut för att nollställga lösenordet med skapat lösenordsbevis",
 	"PasswordResetSuccess":            "Nollställning av lösenord—lyckades!",
 	"PasswordResetSuccessMessage":     "Du har nollställt ditt lösenord!",
 	"PasswordResetSuccessLinkMessage": "Ge det ett försök och",
@@ -192,9 +169,6 @@ var Swedish = map[string]string{
 	"ThreadStartNew": "Starta ny tråd",
 
 	"RegisterHTMLMessage":               `Du har nu ett konto! Välkommen. Besök <a href="/">trådindexet</a> för att läsa och svara på trådar, eller för att starta en ny.`,
-	"RegisterKeypairExplanationStart":   `En grej till: <b>spara nyckeln du ser nedan</b>. Det är ett <a href="https://en.wikipedia.org/wiki/Public-key_cryptography">nyckelpar</a> som tillhandahåller din forumidentitet, och inkluderar en hemlig del som bara du vet om och endast visas nu; forumdatabasen kommer enbart ihåg den publika delen.`,
-	"RegisterViewKeypairExplanationEnd": `Med detta nyckelpar kan du återställa ditt lösenord om du skulle tappa bort det—och detta utan att behöva samla in din email (eller kräva emailinfrastruktur på forumets sida).`,
-	"RegisterKeypairWarning":            "Detta nyckelpar visas enbart denna gång",
 
 	"RegisterVerificationCode":              "Din verifikationskod är",
 	"RegisterVerificationInstructionsTitle": "Verification instructions",
@@ -204,20 +178,6 @@ var Swedish = map[string]string{
 
 	"PasswordResetDescription":            "På denna sida går vi igenom ett par steg för att säkert nollställa ditt lösenord—utan att behöva ta till mejl!",
 	"PasswordResetUsernameQuestion":       "För de första: hur löd användarnamnet?",
-	"PasswordResetCopyPayload":            `Kopiera nu textsnutten nedan (aka <i>beviset</i>)`,
-	"PasswordResetFollowToolInstructions": `Följ <b>verktygsinstruktionerna</b> för att finalisera nollställningen.`,
-	"ToolInstructions":                    `verktygsinstruktionerna`,
-	"PasswordResetToolInstructions": fmt.Sprintf(`
-    <ul>
-        <li><a href="%s">Ladda ned verktyget</a></li>
-        <li>Kör det så hör:<br><code>pwtool --payload &lt;payload från ovan&gt; --keypair &lt;filvägen innehållandes ditt nyckelpar från när du registrerade dig&gt;</code>
-        </li>
-        <li>Kopiera det genererade beviset och klistra in nedan</li>
-        <li>(Kom ihåg att spara ditt lösenord:)</li>
-    </ul>
-    `, toolURL),
-	"GeneratePayload": "skapa payload",
-	"Proof":           "bevis",
 	"NewPassword":     "nytt lösenord",
 	"ChangePassword":  "ändra lösenord",
 }
@@ -246,8 +206,7 @@ var EspanolMexicano = map[string]string{
 	"Username":                  "usuarie",
 	"Current":                   "current",
 	"New":                       "new",
-	"GenerateNewKeypair":        "I also want to generate a new keypair",
-	"ChangePasswordDescription": "Use this page to change your password. If needed, you can also regenerate your password reset keypair—used to reset a forgotten password without admin help.",
+	"ChangePasswordDescription": "Use this page to change your password.",
 	"Password":                  "contraseña",
 	"PasswordMin":               "Debe tener por lo menos 9 caracteres.",
 	"PasswordForgot":            "Olvidaste tu contraseña?",
@@ -264,7 +223,6 @@ var EspanolMexicano = map[string]string{
 	"TextareaPlaceholder": "Tabula rasa",
 
 	"PasswordReset":                   "reset password",
-	"PasswordResetMessage":            "You are logged in, log out to reset password using proof",
 	"PasswordResetSuccess":            "Reset password—success!",
 	"PasswordResetSuccessMessage":     "You reset your password!",
 	"PasswordResetSuccessLinkMessage": "Give it a try and",
@@ -297,9 +255,6 @@ var EspanolMexicano = map[string]string{
 	"YourAnswer":            "Your answer",
 
 	"RegisterHTMLMessage":               `You now have an account! Welcome. Visit the <a href="/">index</a> to read and reply to threads, or start a new one.`,
-	"RegisterKeypairExplanationStart":   `There's just one more thing: <b>save the key displayed below</b>. It is a <a href="https://en.wikipedia.org/wiki/Public-key_cryptography">keypair</a> describing your forum identity, with a private part that only you know; the forum only stores the public portion.`,
-	"RegisterViewKeypairExplanationEnd": `With this keypair you will be able to reset your account if you ever lose your password—and without having to share your email (or require email infrastructure on the forum's part).`,
-	"RegisterKeypairWarning":            "This keypair will only be displayed once",
 
 	"RegisterVerificationCode":              "Your verification code is",
 	"RegisterVerificationInstructionsTitle": "Verification instructions",
@@ -309,20 +264,6 @@ var EspanolMexicano = map[string]string{
 
 	"PasswordResetDescription":            "On this page we'll go through a few steps to securely reset your password—without resorting to any emails!",
 	"PasswordResetUsernameQuestion":       "First up: what was your username?",
-	"PasswordResetCopyPayload":            `Now, first copy the snippet (aka <i>proof payload</i>) below`,
-	"PasswordResetFollowToolInstructions": `Follow the <b>tool instructions</b> to finalize the password reset.`,
-	"ToolInstructions":                    `tool instructions`,
-	"PasswordResetToolInstructions": fmt.Sprintf(`
-    <ul>
-        <li><a href="%s">Download the tool</a></li>
-        <li>Run as:<br><code>pwtool --payload &lt;proof payload from above&gt; --keypair &lt;path to file with yr keypair from registration&gt;</code>
-        </li>
-        <li>Copy the generated proof and paste below</li>
-        <li>(Remember to save your password :)</li>
-    </ul>
-    `, toolURL),
-	"GeneratePayload": "generate payload",
-	"Proof":           "proof",
 	"NewPassword":     "new password",
 	"ChangePassword":  "change password",
 }
--- server/server.go
+++ server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"html/template"
@@ -51,7 +50,6 @@ type PasswordResetData struct {
 
 type ChangePasswordData struct {
 	Action  string
-	Keypair string
 }
 
 type IndexData struct {
@@ -72,10 +70,6 @@ type RegisterData struct {
 	Rules                    template.HTML
 	VerificationInstructions template.HTML
 	ConductLink              string
-}
-
-type RegisterSuccessData struct {
-	Keypair string
 }
 
 type LoginData struct {
@@ -480,23 +474,6 @@ func (h RequestHandler) handleChangePassword(res http.ResponseWriter, req *http.
 		case "/reset/submit":
 			oldPassword := req.PostFormValue("password-old")
 			newPassword := req.PostFormValue("password-new")
-			resetKeypair := (req.PostFormValue("reset-keypair") == "true")
-			var keypairString string
-
-			// check if we're resetting keypair
-			if resetKeypair {
-				// if so: generate new keypair
-				kp, err := crypto.GenerateKeypair()
-				ed.Check(err, "generate keypair")
-				kpBytes, err := kp.Marshal()
-				ed.Check(err, "marshal keypair")
-				pubkey, err := kp.PublicString()
-				ed.Check(err, "get pubkey string")
-				// and set it in db
-				err = h.db.SetPubkey(uid, pubkey)
-				ed.Check(err, "set new pubkey in database")
-				keypairString = string(kpBytes)
-			}
 
 			// check that the submitted, old password is valid
 			username, err := h.db.GetUsername(uid)
@@ -526,7 +503,7 @@ func (h RequestHandler) handleChangePassword(res http.ResponseWriter, req *http.
 			// then save the hash
 			h.db.UpdateUserPasswordHash(uid, pwhashNew)
 			// render a success message & show a link to the login page :')
-			h.renderView(res, "change-password-success", TemplateData{HasRSS: h.config.RSS.URL != "", LoggedIn: true, Data: ChangePasswordData{Keypair: keypairString}})
+			h.renderView(res, "change-password-success", TemplateData{HasRSS: h.config.RSS.URL != "", LoggedIn: true, Data: ChangePasswordData{}})
 		default:
 			fmt.Printf("unsupported POST route (%s), redirecting to /\n", req.URL.Path)
 			IndexRedirect(res, req)
@@ -538,130 +515,28 @@ func (h RequestHandler) handleChangePassword(res http.ResponseWriter, req *http.
 }
 
 func (h RequestHandler) ResetPasswordRoute(res http.ResponseWriter, req *http.Request) {
-	ed := util.Describe("password proof route")
 	loggedIn, _ := h.IsLoggedIn(req)
 	title := util.Capitalize(h.translator.Translate("PasswordReset"))
 
-	// change password functionality, handle this in another function
+	// the user was logged in, let them change their password themselves :)
 	if loggedIn {
 		h.handleChangePassword(res, req)
 		return
 	}
 
-	renderErr := func(errFmt string, args ...interface{}) {
+	renderPlaceholder := func(errFmt string, args ...interface{}) {
 		errMessage := fmt.Sprintf(errFmt, args...)
 		fmt.Println(errMessage)
 		data := GenericMessageData{
 			Title:    title,
 			Message:  errMessage,
-			Link:     "/reset",
+			Link:     "/",
 			LinkText: h.translator.Translate("GoBack"),
 		}
 		h.renderView(res, "generic-message", TemplateData{Data: data, Title: title})
 	}
-
-	switch req.Method {
-	case "GET":
-		switch req.URL.Path {
-		case "/reset/submit":
-			params := req.URL.Query()
-			getParam := func(key string) string {
-				if q, exists := params[key]; exists {
-					return q[0]
-				}
-				fmt.Println("can't find param", key)
-				return ""
-			}
-			username := getParam("username")
-			payload := getParam("payload")
-			h.renderView(res, "password-reset", TemplateData{Data: PasswordResetData{Action: "/reset/submit", Username: username, Payload: payload}})
-		default:
-			h.renderView(res, "password-reset", TemplateData{Data: PasswordResetData{Action: "/reset/generate"}})
-		}
-	case "POST":
-		username := req.PostFormValue("username")
-		switch req.URL.Path {
-		case "/reset/generate":
-			constructProofPayload := func() string {
-				return fmt.Sprintf("%s::%s", username, crypto.GenerateNonce())
-			}
-			payload := constructProofPayload()
-			params := fmt.Sprintf("?payload=%s&username=%s", payload, username)
-			http.Redirect(res, req, "/reset/submit"+params, http.StatusSeeOther)
-		case "/reset/submit":
-			password := req.PostFormValue("password")
-			proofString := req.PostFormValue("proof")
-			payload := req.PostFormValue("payload")
-
-			// make sure the user exists
-			userid, err := h.db.GetUserID(username)
-			if err != nil {
-				renderErr("Wrong username, or a non-existent user")
-				return
-			}
-
-			// make sure the nonce / payload is not being reused
-			nonceExisted, err := h.db.CheckNonceExists(payload)
-			if err != nil {
-				dump(ed.Eout(err, "check nonce existed"))
-				return
-			}
-			if nonceExisted {
-				renderErr("This payload has already been used, please generate a new one")
-				return
-			}
-
-			// get the pubkey, as it is saved in the database for the corresponding user
-			pubkeyString, err := h.db.GetPubkey(userid)
-			if err != nil {
-				renderErr("No matching pubkey found")
-				return
-			}
-			// convert to ed25519.PublicKey
-			pubkey := crypto.PublicKeyFromString(pubkeyString)
-
-			proof, err := hex.DecodeString(proofString)
-			if err != nil {
-				renderErr("The proof format was incorrect")
-				return
-			}
-
-			correct := crypto.VerifyProof(pubkey, []byte(payload), proof)
-			if !correct {
-				renderErr("The proof was incorrect")
-				return
-			}
-			// proof was correct!
-			// save the nonce, so it's not reused
-			err = h.db.AddNonce(payload)
-			if err != nil {
-				dump(ed.Eout(err, "insert nonce into database"))
-				return
-			}
-			// let's set the new password in the database. first, hash it
-			pwhash, err := crypto.HashPassword(password)
-			if err != nil {
-				dump(ed.Eout(err, "hash password during reset"))
-				return
-			}
-			h.db.UpdateUserPasswordHash(userid, pwhash)
-			// render a success message & show a link to the login page :')
-			data := GenericMessageData{
-				Title:       h.translator.Translate("PasswordResetSuccess"),
-				Message:     h.translator.Translate("PasswordResetSuccessMessage"),
-				Link:        "/login",
-				LinkMessage: h.translator.Translate("PasswordResetSuccessLinkMessage"),
-				LinkText:    h.translator.Translate("Login"),
-			}
-			h.renderView(res, "generic-message", TemplateData{Data: data, Title: title})
-		default:
-			fmt.Printf("unsupported POST route (%s), redirecting to /\n", req.URL.Path)
-			IndexRedirect(res, req)
-		}
-	default:
-		fmt.Println("non get/post method, redirecting to index")
-		IndexRedirect(res, req)
-	}
+	renderPlaceholder("Password reset under construction: please contact admin if you need help resetting yr pw :)")
+	return
 }
 
 func (h RequestHandler) RegisterRoute(res http.ResponseWriter, req *http.Request) {
@@ -764,22 +639,7 @@ func (h RequestHandler) RegisterRoute(res http.ResponseWriter, req *http.Request
 		if err = ed.Eout(err, "add registration"); err != nil {
 			dump(err)
 		}
-		// generate and pass public keypair
-		keypair, err := crypto.GenerateKeypair()
-		ed.Check(err, "generate keypair")
-		// record generated pubkey in database for eventual later use
-		pub, err := keypair.PublicString()
-		if err = ed.Eout(err, "convert pubkey to string"); err != nil {
-			dump(err)
-		}
-		ed.Check(err, "stringify pubkey")
-		err = h.db.AddPubkey(userID, pub)
-		if err = ed.Eout(err, "insert pubkey in db"); err != nil {
-			dump(err)
-		}
-		kpJson, err := keypair.Marshal()
-		ed.Check(err, "marshal keypair")
-		h.renderView(res, "register-success", TemplateData{Data: RegisterSuccessData{string(kpJson)}, HasRSS: h.config.RSS.URL != "", LoggedIn: loggedIn, Title: h.translator.Translate("RegisterSuccess")})
+		h.renderView(res, "register-success", TemplateData{HasRSS: h.config.RSS.URL != "", LoggedIn: loggedIn, Title: h.translator.Translate("RegisterSuccess")})
 	default:
 		fmt.Println("non get/post method, redirecting to index")
 		IndexRedirect(res, req)
