220cb6d09cf32addd37d1fe4f48ad1bcf1219531
Milo Fultz <milopfultz@gmail.com>
2023-12-24 14:34:20 -0800

    Rename for organization

--- server/server.go
+++ server/server.go
@@ -240,7 +240,7 @@ func generateTemplates(config types.Config, translator i18n.Translator) (*templa
 		"password-reset",
 		"change-password",
 		"change-password-success",
-		"submit-on-c-enter",
+		"script.submit-on-c-enter",
 	}
 
 	rootTemplate := template.New("root")
