777230730b9e1fb39095c7ccf256035f05f4964d
cblgh <cblgh@cblgh.org>
2025-05-03 09:10:35 +0200

    feat(cerca/cmd): add cerca genauthkey util
    as part of attending to first setup friction (#101), this commit implements a
    util that can be invoked as:
    
        cerca genauthkey
    
    which outputs:
    
        Generated a random key:
        --authkey 14b55e1615bff41ca0bb217a4916e1f7341df07740e7f802c5eb973c573b62b3
    
    the output can be copied and used directly with cerca to serve
    a fresh instance, e.g:
    
        cerca --authkey 14b55e1615bff41ca0bb217a4916e1f7341df07740e7f802c5eb973c573b62b3

---
+++ cmd/cerca/authkey.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"cerca/crypto"
+	"crypto/sha256"
+	"flag"
+)
+
+
+func authkey() {
+	authkeyFlags := flag.NewFlagSet("adduser", flag.ExitOnError)
+
+	help := createHelpString("authkey", []string{
+		`cerca authkey <no other args>`,
+	})
+	authkeyFlags.Usage = func() { usage(help, authkeyFlags) }
+
+	hashInput := []byte(crypto.GeneratePassword())
+	h := sha256.New()
+	h.Write(hashInput)
+	inform("Generated a random key:")
+	inform("--authkey %x", h.Sum(nil))
+}
